07334272d5496ccf49d8bf63793784a8e151d1db
Tomasz Odrzywolek <tomeko92@gmail.com>
2017-10-15 12:06:16 +0200

    add set_path function and some comments

--- main.cpp
+++ main.cpp
@@ -4,63 +4,58 @@
 #include <shlobj.h>
 #include <Shlwapi.h>
 #include <tchar.h>
+#include <iostream>
 
+BOOL set_path(TCHAR* path);
 HANDLE find_files(TCHAR* szDir, WIN32_FIND_DATA* ffd);
 int delete_files(TCHAR* szDir);
 
 int main ()
 {
-    TCHAR szPath[MAX_PATH];
-
-    if(SUCCEEDED(SHGetFolderPath(NULL,
-                             CSIDL_COMMON_APPDATA,
-                             NULL,
-                             0,
-                             szPath)))
+    // empty path
+    TCHAR path[MAX_PATH];
+    // set path to Program Data and check if succeeded
+    if ( set_path(path) )
     {
-        puts( "Udalo sie!" );
-        PathAppend(szPath, TEXT("FluidDesk"));
+        puts( "Sciezka ustalona!" );
+        // add path to FluidDesk folder
+        PathAppend(path, TEXT("FluidDesk"));
 
-        delete_files(szPath);
 
+
+
+
+        int result = delete_files(szPath);
+        std::cout << "Error= " << result;
     }
     else
-        puts( "Error" );
-/*
+        puts( "Path not found" );
 
-
-        if( remove(szPath ) != 0 )
-            perror( "Error deleting file" );
-        else
-            puts( "File successfully deleted" );
-
-
-
-        PathAppend(szPath, TEXT("New Doc.txt"));
-        HANDLE hFile = CreateFile(szPath, ...);
-
-    HRESULT result = SHGetFolderPathA(FOLDERID_ProgramData,
-                                   0,
-                                   NULL,
-                                   program_data);
-
-
-
-*/
-    // free memory in program_data
   return 0;
 }
+
+BOOL set_path(TCHAR* path)
+{
+    return SUCCEEDED(SHGetFolderPath(NULL,
+                                 CSIDL_COMMON_APPDATA,
+                                 NULL,
+                                 0,
+                                 path));
+}
+
+
 
 int delete_files(TCHAR* szDir)
 {
     HANDLE hfind = INVALID_HANDLE_VALUE;
     WIN32_FIND_DATA ffd;
+    DWORD dwerror;
 
     hfind = find_files(szDir, &ffd);
 
     TCHAR szPath[MAX_PATH];
 
-    do
+    while (FindNextFile(hfind, &ffd) != 0 and GetLastError() != ERROR_PATH_NOT_FOUND);
     {
         printf(TEXT("  %s <file>\n"), ffd.cFileName);
         _tcscpy(szPath, szDir);
@@ -69,12 +64,10 @@ int delete_files(TCHAR* szDir)
         if (DeleteFile(szPath))
             puts("File deleted");
         else
-            puts("Error");
+            puts("File not found");
     }
-    while (FindNextFile(hfind, &ffd) != 0);
 
-
-    DWORD dwerror = GetLastError();
+    dwerror = GetLastError();
     if (dwerror != ERROR_NO_MORE_FILES)
     {
         puts("Error");
@@ -95,7 +88,7 @@ HANDLE find_files(TCHAR* szDir, WIN32_FIND_DATA* ffd)
     puts(szPath);
 
     if (INVALID_HANDLE_VALUE == hfind){
-        puts("Error");
+        puts("BBBBBB");
     }
     return hfind;
 }
